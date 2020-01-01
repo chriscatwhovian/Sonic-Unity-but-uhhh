140cc93f8d12dd9df204bd1bb06bc3da44eb81ea
chriscatwhovian <35175288+chriscatwhovian@users.noreply.github.com>
2020-01-01 06:05:12 -0800

    Added basic physics files
    if you see spelling or very stupid mistakes. i stayed up all night on new years and it is 6 am. aslo i know i dont put this here put i dont care

---
+++ Assets/Objs/PlayerCreate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCreate : MonoBehaviour
+{
+    public float hsp = 0;
+    public float vsp = 0;
+
+    public float hspl = 16;
+    public float vspl= 16;
+
+    public float acc = 0.046875f;
+    public float dcc = 0.5f;
+
+    public float frc = 0.046875f;
+
+    public float grv = 0.21875f;
+
+    public bool ground = false;
+   
+    // Start is Preferibale for create things
+    void Start()
+    {
+        
+    }
+
+}
---
+++ Assets/Objs/p_physics.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class p_physics : MonoBehaviour
+{
+    public PlayerCreate pc;
+    public bool isGrouafn;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //limits
+        {
+            if (pc.hsp > pc.hspl)
+            {
+                pc.hsp = pc.hspl;
+            }
+
+            if (pc.hsp < -pc.hspl)
+            {
+                pc.hsp = -pc.hspl;
+            }
+
+            if (pc.vsp > pc.vspl)
+            {
+                pc.vsp = pc.vspl;
+            }
+
+            if (pc.vsp < -pc.vspl)
+            {
+                pc.vsp = -pc.vspl;
+            }
+        }
+
+        //h movement
+        {
+            if (pc.hsp > 0)
+            {
+                int i;
+                for (i = 0; i < pc.hsp; i += 1)
+                {
+                    gameObject.transform.position += new Vector3(1* Time.deltaTime/2, 0, 0);
+
+                }
+            }
+
+            if (pc.hsp < 0)
+            {
+                int i;
+                for (i = 0; i < pc.hsp; i -= 1)
+                {
+                    gameObject.transform.position += new Vector3(-1* Time.deltaTime/2, 0, 0);
+
+                }
+            }
+        }
+
+        //v movement
+        {
+            if (pc.vsp > 0)
+            {
+                int i;
+                for (i = 0; i < pc.vsp && !isGrouafn; i += 1)
+                {
+                    gameObject.transform.position += new Vector3(0, -1 * Time.deltaTime/2, 0);
+
+                }
+            }
+
+
+            if (pc.vsp < 0)
+            {
+                int i;
+                for (i = 0; i < pc.vsp && !isGrouafn; i -= 1)
+                {
+                    gameObject.transform.position += new Vector3(0, 1* Time.deltaTime/2, 0);
+
+                }
+            }
+        }
+
+
+        //land
+        {
+            if (pc.vsp >= 0 && !pc.ground && isGrouafn)
+            {
+                pc.vsp = 0;
+                pc.ground = true;
+            }
+        }
+
+        //Leave Ground
+        {
+            if(isGrouafn == false && pc.ground)
+            {
+                pc.ground = false;
+            }
+        }
+
+        //gravity
+        if (!pc.ground)
+        {
+            pc.vsp += pc.grv;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrouafn = true;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        isGrouafn = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrouafn = false;
+    }
+}
+   
